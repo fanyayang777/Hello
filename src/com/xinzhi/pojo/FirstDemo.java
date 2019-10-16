5048f6bf2d84452436bb4b604ef0162754779095
fanyayang777 <fanyayang777@163.com>
2019-10-16 11:37:35 +0800

    提交第二次文件

--- src/com/xinzhi/pojo/FirstDemo.java
+++ src/com/xinzhi/pojo/FirstDemo.java
@@ -3,5 +3,6 @@ package com.xinzhi.pojo;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("zh");
     }
 }
