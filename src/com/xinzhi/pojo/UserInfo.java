24cca047323a70f2086cd17e6ad4c9cb3112d07a
fanyayang777 <fanyayang777@163.com>
2019-10-16 11:28:08 +0800

    提交第二次文件

---
+++ src/com/xinzhi/pojo/UserInfo.java
@@ -0,0 +1,23 @@
+package com.xinzhi.pojo;
+
+public class UserInfo {
+    private int username;
+    private int password;
+
+    public int getUsername() {
+        return username;
+    }
+
+    public void setUsername(int username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+}
